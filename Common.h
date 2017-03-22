bdb76efcb3ee4bbffb4925964537f4eefe43ca11
Marcus Bauer <mabako@gmail.com>
2017-03-22 11:38:00 +0100

    updated Common.h for x64 stuff

--- Common.h
+++ Common.h
@@ -24,9 +24,25 @@ extern "C"
 }
 
 #ifdef WIN32
-    #define MTAEXPORT extern "C" __declspec(dllexport)
+	#define MTAEXPORT extern "C" __declspec(dllexport)
 #else
-    #define MTAEXPORT extern "C"
+	#define MTAEXPORT extern "C"
+#endif
+
+#if defined( _M_X64 ) || defined( __x86_64__ ) || defined( _M_AMD64 )
+    #define ANY_x64
+    #ifdef _WIN64
+        #define WIN_x64
+    #else
+        #define LINUX_x64
+    #endif
+#else
+    #define ANY_x86
+    #ifdef WIN32
+        #define WIN_x86
+    #else
+        #define LINUX_x86
+    #endif
 #endif
 
 #include <list>
@@ -42,22 +58,22 @@ using namespace std;
 #define MAX_ARGUMENTS 10
 struct FunctionArguments
 {
-    lua_State* luaVM;
-    unsigned char nArguments;
-    unsigned char Type[10];
-    void*   Arguments[10];
+	lua_State* luaVM;
+	unsigned char nArguments;
+	unsigned char Type[10];
+	void*	Arguments[10];
 };
 
 namespace FunctionArgumentType
 {
-    enum
-    {
-        TYPE_NUMBER = 1,
-        TYPE_STRING = 2,
-        TYPE_LIGHTUSERDATA = 3,
-        TYPE_BOOLEAN = 4,
-        TYPE_NIL = 5,
-        TYPE_TABLE = 6
-    };
+	enum
+	{
+		TYPE_NUMBER = 1,
+		TYPE_STRING = 2,
+		TYPE_LIGHTUSERDATA = 3,
+		TYPE_BOOLEAN = 4,
+		TYPE_NIL = 5,
+		TYPE_TABLE = 6
+	};
 }
 #endif
